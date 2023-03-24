0891d8559af6a2940c138a1abc3aa8a298ae5ec2
seyistry <seyistry@gmail.com>
2023-03-24 22:24:10 +0100

    fix image overflow on window

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         };
     }, []);
     return (
-        <section className="relative w-full h-screen mx-auto">
+        <section className="relative w-full h-screen mx-auto overflow-hidden">
             <div
                 className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
             >
